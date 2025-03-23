545644f25d9b57ba4ad88ee27664c521993fd341
Kolam <kolamp@proton.me>
2025-03-24 01:12:00 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@tailwindcss/postcss'],
+  // other config options...
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
