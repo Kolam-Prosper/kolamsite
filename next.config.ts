f4569ce812c1f226ba7b764ef9245ce057561832
Kolam <kolamp@proton.me>
2025-03-24 01:49:30 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@tailwindcss/postcss'],
-  // other config options...
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // This is important for Netlify
+  trailingSlash: true,
+  // Only needed if you're using the App Router
+  images: {
+    unoptimized: true,
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
+
